6cc237d9e6df8cb95105ed2b934c259b7befae6a
Andres Galindo <99367211+andres-galindo@users.noreply.github.com>
2024-03-11 22:02:10 -0500

    Update script.js
    se mejora las funcionalidades de la herramienta

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 let principal = document.querySelector(".principal");
+let modal = document.querySelector(".modal");
+let btn = document.querySelector(".btn");
+let nparte = document.querySelector(".nparte");
+let equipo = document.querySelector(".equipo");
+let compañia = "TSI COLOMBIAS SAS";
+let notificaciones = document.querySelector(".notificaciones");
 
-principal.addEventListener("click", ()=>{
-    let nparte = prompt("NUMERO DE PARTE")
-    let nombre = "TSI COLOMBIAS SAS";
-    let equipo = prompt("NOMBRE EQUIPO")
+btn.addEventListener("click", ()=>{
     
-    for (let i = 0; i <= 39; i++) {
-        let div = document.createElement("div");
-        div.classList.add("items");
-        let empresa = document.createElement("h1");
-        div.appendChild(empresa);
-        empresa.innerText = nombre
-        let h2 = document.createElement("h2");
-        div.appendChild(h2);
-        h2.innerText = nparte
-        let h3 = document.createElement("h3");
-        div.appendChild(h3);
-        h3.innerText = equipo
-        principal.appendChild(div);
+    if(nparte.value === "" && equipo.value === ""){
+        notificaciones.classList.remove("activo");
+    }else{
+        for (let i = 0; i <= 39; i++) {
+            let div = document.createElement("div");
+            div.classList.add("items");
+            let empresa = document.createElement("h1");
+            div.appendChild(empresa);
+            empresa.innerText = compañia
+            let h2 = document.createElement("h2");
+            div.appendChild(h2);
+            h2.innerText = nparte.value;
+            let h3 = document.createElement("h3");
+            div.appendChild(h3);
+            h3.innerText = equipo.value
+            principal.appendChild(div);
+        }
+        principal.style.background = "white";
+        modal.classList.add("activo");
+        notificaciones.classList.add("activo");
     }
 })
